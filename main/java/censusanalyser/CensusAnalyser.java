4bce81a665a357112e6673e5f3fc83f543c27029
Shweta2105 <sghadi21@gmail.com>
2021-07-02 09:16:55 +0530

    while loop changed to iterator

--- main/java/censusanalyser/CensusAnalyser.java
+++ main/java/censusanalyser/CensusAnalyser.java
@@ -3,11 +3,13 @@ package censusanalyser;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
+import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
@@ -17,16 +19,17 @@ public class CensusAnalyser {
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
-            int namOfEateries = 0;
-            while (censusCSVIterator.hasNext()) {
-                namOfEateries++;
-                IndiaCensusCSV censusData = censusCSVIterator.next();
-            }
-            return namOfEateries;
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+            Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
+
+           return (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }catch (IllegalArgumentException e) {
+            throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.UN_PROCCESSABLE);
+
         }
     }
 }
